4eb6bda46aa3a0ada411283646d519991bdb9a61
birdman769 <48811918+birdman769@users.noreply.github.com>
2022-08-19 12:41:16 +1000

    updated the login and register form to include the use of useRef for nicer User experience in regards to focusing on incorrect fields

--- s3482450-a1/src/components/RegisterForm.js
+++ s3482450-a1/src/components/RegisterForm.js
@@ -1,6 +1,6 @@
-import { useState } from "react";
-import { Form, Button, Row, Col, Alert} from "react-bootstrap";
-import { Link,  useNavigate } from "react-router-dom";
+import { useRef, useState } from "react";
+import { Form, Button, Row, Col, Alert } from "react-bootstrap";
+import { Link, useNavigate } from "react-router-dom";
 import "./Login.css";
 const RegisterForm = (props) => {
   const [email, setEmail] = useState("");
@@ -10,69 +10,69 @@ const RegisterForm = (props) => {
 
   const navigate = useNavigate();
 
-
+  const emailRef = useRef(null);
+  const passRef = useRef(null);
+  const nameRef = useRef(null);
 
   //   email regex from : https://emailregex.com/
 
   const EMAIL_REGEX =
     /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-//   password regex from: https://www.thepolyglotdeveloper.com/2015/05/use-regex-to-test-password-strength-in-javascript/
+  //   password regex from: https://www.thepolyglotdeveloper.com/2015/05/use-regex-to-test-password-strength-in-javascript/
 
-  
-    const PASS_REGEX =  new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,})");
-
+  const PASS_REGEX = new RegExp(
+    "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,})"
+  );
 
   const handleSubmit = (e) => {
     setError("");
 
     e.preventDefault();
-    if(name.length <1 ){
-        setError("insert a name!");
-        return;
-
+    if (name.length < 1) {
+      setError("insert a name!");
+      nameRef.current.focus();
+      return;
     }
     if (!EMAIL_REGEX.test(email)) {
       setError("incorrect email format!");
+      emailRef.current.focus();
+
       return;
     }
- 
+
     if (!PASS_REGEX.test(password)) {
-  
-      setError("password must be at least 8 characters, contain at least one numeric digit, one special character, one uppercase and one lowercase letter");
+      setError(
+        "password must be at least 8 characters, contain at least one numeric digit, one special character, one uppercase and one lowercase letter"
+      );
+      passRef.current.focus();
+
       return;
     }
 
-    registerUser(name,email,password);
-    navigate("/loginForm")
-
-
+    registerUser(name, email, password);
+    navigate("/loginForm");
   };
 
-  const registerUser = (name, email, password)=>{
- 
+  const registerUser = (name, email, password) => {
     localStorage.setItem("name", name);
     localStorage.setItem("email", email);
     localStorage.setItem("password", password);
-  
-  }
+  };
 
   return (
     <div>
       {" "}
-      <Form onSubmit= {handleSubmit}>
-      <Form.Group
+      <Form onSubmit={handleSubmit}>
+        <Form.Group
           className="mb-3"
           controlId="formBasicName"
           value={name}
           onChange={(e) => setName(e.target.value)}
-          
         >
           <Form.Label>Name</Form.Label>
-          <Form.Control type="text" placeholder="john smith" />
-          <Form.Text className="text-muted">
-            name goes here
-          </Form.Text>
+          <Form.Control type="text" placeholder="john smith" ref={nameRef} />
+          <Form.Text className="text-muted">name goes here</Form.Text>
         </Form.Group>
         <Form.Group
           className="mb-3"
@@ -81,7 +81,11 @@ const RegisterForm = (props) => {
           onChange={(e) => setEmail(e.target.value)}
         >
           <Form.Label>Email address</Form.Label>
-          <Form.Control type="text" placeholder="email@email.com" />
+          <Form.Control
+            type="text"
+            placeholder="email@email.com"
+            ref={emailRef}
+          />
           <Form.Text className="text-muted">
             your email and Soul belongs to the Lan corperation from now
           </Form.Text>
@@ -95,6 +99,7 @@ const RegisterForm = (props) => {
             placeholder="Password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
+            ref={passRef}
           />
           <Form.Text className="text-muted">
             password must be at least 8 characters- praise LAN.
@@ -108,9 +113,15 @@ const RegisterForm = (props) => {
             </Button>
           </Col>
           <Col>
-           <Link to= "/"> <Button className="button-bigger" variant="secondary" type="submit">
-              Back
-            </Button>
+            <Link to="/">
+              {" "}
+              <Button
+                className="button-bigger"
+                variant="secondary"
+                type="submit"
+              >
+                Back
+              </Button>
             </Link>
           </Col>
         </Row>
