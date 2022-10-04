65c8ed4b50b67ff2747d562b4f6d578c942a5e0c
birdman769 <48811918+birdman769@users.noreply.github.com>
2022-10-04 14:39:34 +1100

    set up the basic database/express with a user table based on the lab08 login-registration example

---
+++ express/src/database/index.js
@@ -0,0 +1,25 @@
+// THIS FILE IS BASED OF THE RMIT-FWP LAB 08- login-registration-example
+
+const { Sequelize, DataTypes } = require("sequelize");
+const config = require("./config.js");
+
+const db = {
+  Op: Sequelize.Op,
+};
+
+// Create Sequelize.
+db.sequelize = new Sequelize(config.DB, config.USER, config.PASSWORD, {
+  host: config.HOST,
+  dialect: config.DIALECT,
+});
+
+// Include models.
+db.user = require("./models/user.js")(db.sequelize, DataTypes);
+
+//sync the database
+db.sync = async () => {
+  // Sync schema.
+  await db.sequelize.sync();
+};
+
+module.exports = db;
---
+++ express/src/database/models/user.js
@@ -0,0 +1,24 @@
+// THIS FILE IS BASED OF THE RMIT-FWP LAB 08- login-registration-example
+
+module.exports = (sequelize, DataTypes) =>
+  sequelize.define(
+    "user",
+    {
+      username: {
+        type: DataTypes.STRING(32),
+        primaryKey: true,
+      },
+      password_hash: {
+        type: DataTypes.STRING(96),
+        allowNull: false,
+      },
+      name: {
+        type: DataTypes.STRING(40),
+        allowNull: false,
+      },
+    },
+    {
+      // I want to have the created at timestamp to show when the user account was created on the profile page
+      timestamps: true,
+    }
+  );
